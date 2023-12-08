62c7b24e192860a231d399cba3aa37e3a3e7525a
Sergio Cruz <vlad.crz@gmail.com>
2023-12-07 23:23:25 -0600

    Adding controller and transaction management.

--- src/main/java/com/vscg/addressservice/AddressServiceApplication.java
+++ src/main/java/com/vscg/addressservice/AddressServiceApplication.java
@@ -2,8 +2,10 @@ package com.vscg.addressservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class AddressServiceApplication {
 
     public static void main(String[] args) {
